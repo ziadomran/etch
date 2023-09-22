f73d0e52c6ab7539025816d2e0dd7af05ee6b5c9
ziadomran <daiznarmo@hotmail.com>
2023-09-22 13:23:56 +0300

    Add Style from JavaScript. Finally fixed  the problem

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const container = document.querySelector('#container');
+// const rows = document.querySelector(".row");
+// const cols = document.querySelection(".col");
 
-for(let i=0; i<16; i++){
-    for(let j=0; j<16; j++){
-        const create = document.createElement('div')
-        create.classList.add('square')
-       
+function makeGrid(number){
+for(let i=0; i<number * number; i++){ //create rows
+    
+        const create = document.createElement('div');
+        // create.classList.add('square');
+        // const square = document.querySelectorAll('.square');
+        // create.style.display = "grid";
+        container.style.gridTemplateColumns = `repeat(${number}, auto)`;
+        container.style.gridTemplateRows = `repeat(${number}, auto)`;
+        container.style.width = `960px`;
+        container.style.height = `960px`;
+        create.addEventListener('mouseover', () => create.style.backgroundColor = 'black');
         container.appendChild(create);
+        
+    
+}
+}
+
+function createRow(number){
+for (let i=0; i<number; i++){ 
+    const create = document.createElement('div');
+    create.classList.add('row');
+    create.style.g
+}
+}
+
+
+// Function to clear  the grid to take new data
+function clearGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
 }
 
+
+const button = document.querySelector('.number')
+button.addEventListener('click', () => {
+    const newNumber = prompt("Enter a number: ")
+    clearGrid();
+    makeGrid(newNumber);
+})
