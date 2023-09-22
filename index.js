046d11f3920bb71ad6f35bb4fb4bac8a2124fc02
ziadomran <daiznarmo@hotmail.com>
2023-09-22 17:43:15 +0300

    Clean Code

--- index.js
+++ index.js
@@ -3,31 +3,16 @@ const container = document.querySelector('#container');
 // const cols = document.querySelection(".col");
 
 function makeGrid(number){
-for(let i=0; i<number * number; i++){ //create rows
-    
+    for(let i=0; i<number * number; i++){ 
         const create = document.createElement('div');
-        // create.classList.add('square');
-        // const square = document.querySelectorAll('.square');
-        // create.style.display = "grid";
-        container.style.gridTemplateColumns = `repeat(${number}, auto)`;
-        container.style.gridTemplateRows = `repeat(${number}, auto)`;
-        container.style.width = `960px`;
-        container.style.height = `960px`;
+        container.style.gridTemplateColumns = `repeat(${number}, auto)`; // Create "Number" number of columns
+        container.style.gridTemplateRows = `repeat(${number}, auto)`; //create "Number" number of rows
+        container.style.width = `960px`; //grid width 960px
+        container.style.height = `960px`; //grid height 960px
         create.addEventListener('mouseover', () => create.style.backgroundColor = 'black');
         container.appendChild(create);
-        
-    
+    }
 }
-}
-
-function createRow(number){
-for (let i=0; i<number; i++){ 
-    const create = document.createElement('div');
-    create.classList.add('row');
-    create.style.g
-}
-}
-
 
 // Function to clear  the grid to take new data
 function clearGrid(){
@@ -36,10 +21,21 @@ function clearGrid(){
     }
 }
 
+function checkPrompt(str){
+    if (str === undefined || str === null || isNaN(str))  return alert("Please select a number");
+    else if (!Number.isInteger(str)) return alert("Please select an Integer number")
+    else if (str > 100) return alert("Number is too large, will crash your browser");
+    else return parseInt(str);
+}
 
 const button = document.querySelector('.number')
 button.addEventListener('click', () => {
-    const newNumber = prompt("Enter a number: ")
+    const newNumber = +prompt("Enter a number: ")
+    if (newNumber === undefined || newNumber === null) return alert("Please select a number");
+    else if (!Number.isInteger(newNumber)) return alert("Please select an Integer number")
+    else if (newNumber > 100) return alert("Number is too large, will crash your browser");
+    else{
     clearGrid();
     makeGrid(newNumber);
+    }
 })
