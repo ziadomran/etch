95ade59a077964ed54705ac92699c2aab609ce53
ziadomran <daiznarmo@hotmail.com>
2023-09-26 15:47:24 +0300

    Fix Buttons not Working

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('#container');
-// const rows = document.querySelector(".row");
-// const cols = document.querySelection(".col");
+const color = document.querySelector('.colors');
+const black = document.querySelector('.black');
+const erase = document.querySelector('.erase');
+const clear = document.querySelector('.clear');
+
+let mode;
+
+color.addEventListener("click", () => mode = rainbowMode)
+black.addEventListener("click", () => mode ='blackMode')
+erase.addEventListener("click", () => mode = 'eraseMode')
+clear.addEventListener("click", () => { mode = 'clearMode'
+    clearGrid() });
 
 function makeGrid(number){
     for(let i=0; i<number * number; i++){ 
@@ -9,25 +19,30 @@ function makeGrid(number){
         container.style.gridTemplateRows = `repeat(${number}, auto)`; //create "Number" number of rows
         container.style.width = `960px`; //grid width 960px
         container.style.height = `960px`; //grid height 960px
-        create.addEventListener('mouseover', () => create.style.backgroundColor = 'black');
+        create.addEventListener('mouseover', changeColor);
         container.appendChild(create);
     }
 }
 
-// Function to clear  the grid to take new data
+function randomColor(){
+    return Math.floor(Math.random() *256);
+}
+function rainbowMode(){
+    return `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+}
+
 function clearGrid(){
+    container.childNodes.forEach((node) => node.style.backgroundColor ='white');
+}
+
+// Function to clear  the grid to take new data
+function eraseGrid(){
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
 }
 
-function checkPrompt(str){
-    if (str === undefined || str === null || isNaN(str))  return alert("Please select a number");
-    else if (!Number.isInteger(str)) return alert("Please select an Integer number")
-    else if (str > 100) return alert("Number is too large, will crash your browser");
-    else return parseInt(str);
-}
-
+//Numbers Button
 const button = document.querySelector('.number')
 button.addEventListener('click', () => {
     const newNumber = +prompt("Enter a number: ")
@@ -35,7 +50,16 @@ button.addEventListener('click', () => {
     else if (!Number.isInteger(newNumber)) return alert("Please select an Integer number")
     else if (newNumber > 100) return alert("Number is too large, will crash your browser");
     else{
-    clearGrid();
+    eraseGrid();
     makeGrid(newNumber);
     }
 })
+
+function changeColor(event){
+    if (mode === rainbowMode)
+event.target.style.backgroundColor = rainbowMode();
+else if (mode === 'blackMode')
+    event.target.style.backgroundColor = 'black';
+else if (mode === "eraseMode")
+    event.target.style.backgroundColor = 'white';
+}
